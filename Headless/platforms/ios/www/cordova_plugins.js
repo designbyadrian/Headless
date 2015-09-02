aa9c832328fcc13d895b7e97d6597b6c2e642fe5
Adrian von Gegerfelt <adrian@designbyadrian.com>
2015-09-02 11:04:24 +0200

    added statusbar plugin

--- Headless/platforms/ios/www/cordova_plugins.js
+++ Headless/platforms/ios/www/cordova_plugins.js
@@ -14,6 +14,13 @@ module.exports = [
             "plugin.printer",
             "cordova.plugins.printer"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
+        "id": "org.apache.cordova.statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -21,7 +28,8 @@ module.exports.metadata =
 {
     "com.bez4pieci.cookies": "0.0.1",
     "cordova-plugin-whitelist": "1.0.0",
-    "de.appplant.cordova.plugin.printer": "0.6.2"
+    "de.appplant.cordova.plugin.printer": "0.6.2",
+    "org.apache.cordova.statusbar": "0.1.10"
 }
 // BOTTOM OF METADATA
 });
